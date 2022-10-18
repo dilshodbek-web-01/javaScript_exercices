c38d1d1205721973d18dc1a6ed499eea3323a360
dilshodbek-web-01 <dilshodbekkomilov.012@gmail.com>
2022-10-18 20:44:13 +0500

    javaScript_execices

---
+++ script.js
@@ -0,0 +1,432 @@
+// SWITCH CASE homeworks start
+// #1.
+
+// let count = +prompt("enter number");
+
+// switch (count) {
+//   case 1:
+//     console.log("Dushanba");
+//     break;
+//   case 2:
+//     console.log("Seshanba");
+//     break;
+//   case 3:
+//     console.log("Chorshanba");
+//     break;
+//   case 4:
+//     console.log("Payshanba");
+//     break;
+//   case 5:
+//     console.log("Juma");
+//     break;
+//   case 6:
+//     console.log("Shanba");
+//     break;
+//   case 7:
+//     console.log("Yakshanba");
+//     break;
+//   default:
+//     alert("NOT FOUND");
+// }
+
+// #2.
+
+// let k = +prompt("ballingizni kiriting ?");
+
+// switch (k) {
+//   case 1:
+//     console.log("Yomon");
+//     break;
+//   case 2:
+//     console.log("Qoniqarsiz");
+//     break;
+//   case 3:
+//     console.log("Qoniqarli");
+//     break;
+//   case 4:
+//     console.log("Yaxshi");
+//     break;
+//   case 5:
+//     console.log("Alo");
+//     break;
+//   default:
+//     alert("Baho yoq");
+// }
+
+// #3.
+
+// let month = +prompt("Oy raqamini kiriting");
+
+// switch (month) {
+//   case 1:
+//     console.log("Qish");
+//     break;
+//   case 2:
+//     console.log("Qish");
+//     break;
+//   case 3:
+//     console.log("Bahor");
+//     break;
+//   case 4:
+//     console.log("Bahor");
+//     break;
+//   case 5:
+//     console.log("Bahor");
+//     break;
+//   case 6:
+//     console.log("Yoz");
+//     break;
+//   case 7:
+//     console.log("Yoz");
+//     break;
+//   case 8:
+//     console.log("Yoz");
+//     break;
+//   case 9:
+//     console.log("Kuz");
+//     break;
+//   case 10:
+//     console.log("Kuz");
+//     break;
+//   case 11:
+//     console.log("Kuz");
+//     break;
+//   case 12:
+//     console.log("Qish");
+//     break;
+//   default:
+//     alert("Bunday fasl yo'q");
+// }
+
+// #4.
+
+// let monthdate = +prompt("Oy raqamini kiriting");
+
+// switch (monthdate) {
+//   case 1:
+//     console.log("Yanvar 31 kun");
+//     break;
+//   case 2:
+//     console.log("Fevral 28 yoki 29 kun");
+//     break;
+//   case 3:
+//     console.log("Mart 31 kun");
+//     break;
+//   case 4:
+//     console.log("aprel 30 kun");
+//     break;
+//   case 5:
+//     console.log("May 31 kun");
+//     break;
+//   case 6:
+//     console.log("Iyun 30 kun");
+//     break;
+//   case 7:
+//     console.log("Iyul 31 kun");
+//     break;
+//   case 8:
+//     console.log("Avgust 31 kun");
+//     break;
+//   case 9:
+//     console.log("Sentyabr 30 kun");
+//     break;
+//   case 10:
+//     console.log("Oktabr 31 kun");
+//     break;
+//   case 11:
+//     console.log("Noyabr 30 kun");
+//     break;
+//   case 12:
+//     console.log("Dekabr 31 kun");
+//     break;
+//   default:
+//     alert("Bunday oy yo'q");
+// }
+
+// #5.
+
+// let c = "**";
+// let a = 30;
+// let b = 10;
+// switch (c) {
+//   case "+":
+//     console.log(a + b);
+//     break;
+//   case "-":
+//     console.log(a - b);
+//     break;
+//   case "*":
+//     console.log(a * b);
+//     break;
+//   case "/":
+//     console.log(a / b);
+//     break;
+//   case "**":
+//     console.log(a ** b);
+//     break;
+//   default:
+//     console.log("Bunday qiymat mavjud emas");
+// }
+
+// #6.
+
+// let scholarship = +prompt("Bahoingizni kiriting");
+
+// switch (scholarship) {
+//   case 100:
+//     console.log("500 000 so'm");
+//     break;
+//   case 110:
+//     console.log("600 000 so'm");
+//     break;
+//   case 120:
+//     console.log("700 000 so'm");
+//     break;
+//   case 130:
+//     console.log("800 000 so'm");
+//     break;
+//   case 140:
+//     console.log("900 000 so'm");
+//     break;
+//   case 150:
+//     console.log("1 000 000 so'm");
+//     break;
+//   case 160:
+//     console.log("2 000 000 so'm");
+//     break;
+//   case 170:
+//     console.log("3 000 000 so'm");
+//     break;
+//   case 180:
+//     console.log("4 000 000 so'm");
+//     break;
+//   case 190:
+//     console.log("5 000 000 so'm");
+//     break;
+//   case 200:
+//     console.log("6 000 000 so'm");
+//     break;
+
+//   default:
+//     alert("Bunday qiymat mavjud emas");
+// }
+
+// SWITCH CASE homeworks end
+
+//  FOR. homeworks start
+
+// #1.
+
+// let k = 1;
+// let n = 10;
+// for(let i = k; i <= n; i++){
+//     console.log(i);
+// }
+
+// #2.
+
+// let a = 10;
+// let b = 1;
+// for(let i = b; i <= a; i++){
+//     console.log(i);
+// }
+
+// #3.
+
+// let a = 10;
+// let b = 1;
+// for(let i = a - 1; i > b; i--){
+//     console.log(i)
+// }
+
+// #4.
+
+// let value = 100000;
+// let j = 0;
+// for(let i = 1; i <= 10; i++){
+//     console.log(i + "kg" + " narxi: " + (j+=value) + " value");
+// }
+
+// #5.
+// ????
+
+// #6.
+// ????
+
+// #7.
+
+// let a = 1;
+// let b = 9;
+// let c = 0;
+// for(let i = a; i <= b; i++){
+//     c += i;
+// }
+// console.log(c);
+
+// #8.
+
+// let a = 1;
+// let b = 9;
+// let c = 1;
+// for(let i = a; i <= b; i++){
+//     c *= i;
+// }
+// console.log(c);
+
+// #9.
+
+// let a = 1;
+// let b = 9;
+// let c = 0;
+// for( i = a; i <= b; i++){
+//     c += i * i;
+// }
+// console.log(c)
+
+// #10.
+// ????
+
+// #11.
+
+// for (let i=0; i<=10; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+// #12.
+
+// for (let i=0; i<=10; i++){
+//     if(i%2==1){
+//         console.log(i);
+//     }
+// }
+
+// #13.
+
+// for (let i=4; i<=100; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+// #14.
+// ????
+
+// #15.
+// ????
+
+// #16.
+// ????
+
+// #17.
+// ????
+
+// #18.
+// ????
+
+// #19.
+// let x = Number(prompt("x="));
+// let n = Number(prompt("n="));
+// let price = 1;
+// for (let i = 1; i <= n; i++){
+//   let fac = 1;
+//   for (let j = 1; j <= i; j++){
+//     fac *= j
+//   }
+//   price += Math.pow(x, i)/fac
+// }
+// console.log(price);
+
+// #20.
+
+// for (let i=0; i<=55; i++){
+//     if(i%5==0){
+//         console.log(i);
+//     }
+// }??
+
+// #21.
+// ????
+
+//  FOR HOMEWORKS END
+
+// FOR exercises start
+
+// #1. for karra jadval.
+
+// for (let i=1; i<=10; i++){
+//     for (let j=1; j<=10; j++){
+//         console.log(i+"x"+j+"="+i*j)
+//     }
+// }
+
+// #2.
+
+// for (let i=1; i<=40; i++) {
+//     console.log(i);
+//     if(i==8)
+//     // break
+//     continue
+// }
+
+// let n=40;
+
+// do{
+//     n++
+//     console.log(n);
+// }while(n<10);
+
+// FOR exercises end
+
+// "ARRAY" HOMEWORS START
+
+// #1.
+
+// let n=+prompt("write number and take it");
+// let array=[];
+
+// for(let i=0; i<=n; i++){
+//   array.push(i);
+//   console.log(array[i]);
+// }
+// console.log(array);
+
+// #2.
+
+// let n = +prompt("n");
+// let array = [];
+
+// for (let i = 0; i <= n; i++) {
+//   array.push(2**i);
+//   console.log(array[i]);
+// }
+// console.log(array);
+
+// "ARRAY" HOMEWORS END
+
+// ARRAY exercises start
+// #1.
+// let nums = [0, 1, 5, 3, 9];
+// let arr = [];
+
+// for (let i = 0; i < nums.length; i++) {
+//   if (i == nums[i]) {
+//     arr.push(true);
+//   } else {
+//     arr.push(false);
+//   }
+// }
+// console.log(arr);
+
+// #2.
+// let array=['javaScript', 'go lang', 'css'];
+// console.log(array);
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// for (let i=0; i<=2; i++){
+//   console.log(array[i]);
+// }
+
+// ARRAY exercises end
